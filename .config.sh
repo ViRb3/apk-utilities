058938ec6dfeb4f7fee3ccbd1b196fb6be4c1ece
Victor <virb3e@gmail.com>
2019-02-21 12:33:40 +0000

    Remove dot slash from paths in config

--- .config.sh
+++ .config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export BINDIR="./bin"
+export BINDIR="bin"
 export APKTOOL=$BINDIR/apktool_2.3.4.jar
 export BAKSMALI=$BINDIR/baksmali-2.2.6.jar
 export SMALI=$BINDIR/smali-2.2.6.jar
 export APKSIGNER=$BINDIR/uber-apk-signer-1.0.0.jar
 export ENJARIFY=$BINDIR/enjarify.sh
 
-export WORKDIR="./project"
+export WORKDIR="project"
 
 # is running under WSL?
 if [ $(uname -r | grep "Microsoft") ]
