de2a514d3b12166a61872e3b430fb24037ad3615
Victor <virb3e@gmail.com>
2019-02-21 11:48:46 +0000

    Improve adb pull script

--- adb-pull.sh
+++ adb-pull.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 . ".config.sh"
 
-$ADB shell pm list packages -f -3
+$ADB shell 'pm list packages' | sed 's/.*://' | sort
 echo
 read -p 'Package to pull: ' PKG
-$ADB pull $PKG
+BASE=$($ADB shell pm path ${PKG} | awk -F':' '{print $2}' | tr -d '\r' | tr -d '\n')
+
+if [ -z "$BASE" ]; then
+    echo "Invalid package!"
+    exit 1
+fi
+
 mkdir -p "$WORKDIR"
-mv "base.apk" "$WORKDIR/app.apk"
+$ADB pull "$BASE" "$WORKDIR/app.apk"
