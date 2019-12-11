582b25a90767ec422b5339f8554ba0d8722cbddc
Victor <virb3e@gmail.com>
2019-12-11 16:21:16 +0000

    Interactive search for adb pull

--- adb-pull.sh
+++ adb-pull.sh
@@ -2,10 +2,26 @@
 . ".config.sh"
 DEST="$WORKDIR/app.apk"
 
-$ADB shell 'pm list packages' | sed 's/.*://' | sort
-echo
-read -p 'Package to pull: ' PKG
-BASE=$($ADB shell pm path ${PKG} | awk -F':' '{print $2}' | tr -d '\r' | tr -d '\n')
+search_package () {
+    read -p 'Search for package: ' PKG
+    RESULTS=$($ADB shell 'pm list packages' | sed 's/.*://' | sort | grep "$PKG")
+    if [ -z "$RESULTS" ]; then
+        RESULTS_COUNT=0
+    else
+        RESULTS_COUNT=$(echo "$RESULTS" | wc -l)
+    fi
+    echo "$RESULTS"
+    echo ""
+    echo "$RESULTS_COUNT results"
+}
+
+search_package
+while [ "$RESULTS_COUNT" -ne 1 ]; do
+    search_package
+done
+
+RESULTS=$(echo "$RESULTS" | tr -d '\r' | tr -d '\n')
+BASE=$($ADB shell pm path "$RESULTS" | awk -F':' '{print $2}' | tr -d '\r' | tr -d '\n')
 
 check_empty "$BASE" "Invalid package!"
 
