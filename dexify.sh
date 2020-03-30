2b8f3f8b390d962bf5ee249463d424bfa8b81897
Victor <virb3e@gmail.com>
2020-03-30 17:00:12 +0300

    Use min SDK 26 for dexify

--- dexify.sh
+++ dexify.sh
@@ -4,7 +4,7 @@ DEST="$WORKDIR/app.dex"
 
 check_file "$WORKDIR/app.jar" "app.jar not found! Place your input file there."
 
-$DX --dex --output "$DEST" "$WORKDIR/app.jar"
+$DX --dex --min-sdk-version 26 --output "$DEST" "$WORKDIR/app.jar"
 
 check_status
 echo "Saved to: $DEST"
